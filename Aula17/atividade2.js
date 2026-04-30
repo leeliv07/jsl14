61b91d561358e54653a9324efbbe9fb7335fd757
lívia <liviacostta10@gmail.com>
2026-04-29 22:04:55 -0300

    arrow functions

---
+++ Aula17/atividade2.js
@@ -0,0 +1,12 @@
+//usando o método forEach para percorrer um array mostre as informações de cada pessoa com o seguinte formato: "Nome: [nome], Idade: [idade], Cidade: [cidade], salario: [salário], bonus: [bonus]".
+// o bonus deve ser calculado como 20% do salário
+
+let pessoas = [
+    {nome: "João", idade: 30, cidade: "São Paulo", salario: 3000},
+    {nome: "Maria", idade: 25, cidade: "Rio de Janeiro", salario: 2500},
+    {nome: "Pedro", idade: 35, cidade: "Belo Horizonte", salario: 3500}
+]
+
+//https://github.com/okanharenan/js-l14/blob/main/aula17/atividade2.js
+//https://github.com/alexsousarodrigues1919-max/DC-LV14-JS/blob/main/js/aula17/atividade02.js
+//https://github.com/yagol2202/jsL14/blob/8b45f921ccfe8b746446252ddaace8560db1d7fd/aula16/forEach.js
